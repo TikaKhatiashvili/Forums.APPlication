9068c5eeb8c842f0e2ac603364756cc4c66fa783
Tika Khatiashvili <tikaxatiashvili1997@gmail.com>
2026-02-03 14:08:58 +0400

    implementI baseRepository

--- Forums.API/Repository/BaseRepository.cs
+++ Forums.API/Repository/BaseRepository.cs
@@ -1,48 +1,81 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Forums.API.Repository;
 
 public class BaseRepository<T, TContext> : IBaseRepository<T, TContext> where T : class where TContext : DbContext
 {
-    public async Task AddAsync(T entity)
+    private readonly DbContext _context;
+    private readonly DbSet<T> _dbSet;
+    public BaseRepository(TContext context)
     {
-        throw new NotImplementedException();
+        _context = context;//ბაზა
+        _dbSet = _context.Set<T>();//მიბრუნებს ნებსმიერ DbSet - ს //ცხრილი
+    }
+    public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+    public async Task<int> SaveAsync(CancellationToken cancellationToken = default) => await _context.SaveChangesAsync(cancellationToken);
+    public void Remove(T entity) => _dbSet.Remove(entity);
+    public void RemoveRange(IEnumerable<T> entities) => _dbSet.RemoveRange(entities);
+    public void Update(T entity) => _dbSet.Update(entity);
+
+    public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate) => await _dbSet.AnyAsync(predicate);
+
+    public async Task<(List<T> Items, int TotalCount)> GetAllAsync(
+        Expression<Func<T,
+            bool>> filter = null,
+        int? pageNumber = null,
+        int? pageSize = null,
+        string orderBy = null,
+        bool ascending = true,
+        string includeProperties = null,
+        bool tracking = true)
+    {
+        IQueryable<T> query = _dbSet.Where(filter);
+        if (!tracking)
+
+            query = query.AsNoTracking();
+
+        if (!string.IsNullOrWhiteSpace(includeProperties))
+            query = ApplyIncludes(query, includeProperties);
+
+        int totalCount = await query.CountAsync();
+
+        if (!string.IsNullOrWhiteSpace(orderBy))
+            query = ApplyIncludes(orderBy, ascending, query);
+        if (pageNumber.HasValue && pageSize.HasValue)
+        {
+            int skip = (pageNumber.Value - 1) * pageSize.Value;
+            query = query.Skip(skip).Take(pageSize.Value);
+        }
+        var items=await query.ToListAsync();
+        return(items, totalCount);
     }
 
-    public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate)
+    public async Task<T> GetAsync(Expression<Func<T, bool>> filter, string includeProperties, bool tracking = true)//dbset დამყავს IQueryable<T> მდე
     {
-        throw new NotImplementedException();
-    }
+        IQueryable<T> query = _dbSet.Where(filter);
+        if (!tracking)
+        {
+            query = query.AsNoTracking();
+        }
 
-    public async Task<(List<T> Items, int TotalCount)> GetAllAsync(Expression<Func<T, bool>> filter = null, int? pageNumber = null, int? pageSize = null, string orderBy = null, bool ascending = true, string includeProperties = null, bool tracking = true)
+        query = ApplyIncludes(query, includeProperties);
+        return await query.FirstOrDefaultAsync();
+    }
+    private IQueryable<T> ApplyIncludes(string orderBy, bool ascending, IQueryable<T> query)
     {
         throw new NotImplementedException();
     }
-
-    public async Task<T> GetAsync(Expression<Func<T, bool>> predicate, string includProperties, bool tracking = true)
+    private static IQueryable<T> ApplyIncludes(IQueryable<T> query, string includeProperties)
     {
-        throw new NotImplementedException();
+        if (!string.IsNullOrWhiteSpace(includeProperties))
+        {
+            foreach (var includeProperty in includeProperties.Split(',', StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty.Trim());
+            }
+        }
+        return query;
     }
-
-    public async void Remove(T entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async void RemoveRange(IEnumerable<T> entities)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task<int> SaveAsync(CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async void Update(T entity)
-    {
-        throw new NotImplementedException();
-    }
-    //დრო 01:42:57
 }
--- Forums.API/Repository/IBaseRepository.cs
+++ Forums.API/Repository/IBaseRepository.cs
@@ -13,7 +13,7 @@ public interface IBaseRepository<T, TContext>
     void Update(T entity);
     Task<int>SaveAsync(CancellationToken cancellationToken = default);  
     Task<bool> ExistsAsync(Expression<Func<T,bool>>predicate);
-    Task<T>GetAsync(Expression<Func<T, bool>> predicate,string includProperties,bool tracking=true);
+    Task<T>GetAsync(Expression<Func<T, bool>> filter, string includProperties,bool tracking=true);
     Task<(List<T> Items, int TotalCount)> GetAllAsync(
         Expression<Func<T, bool>> filter = null,
         int ? pageNumber=null,
